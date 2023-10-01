31d56a54822eef3f95721e0bdc67919a4f549e5c
shovon005 <144725632+shovon005@users.noreply.github.com>
2023-10-01 21:29:00 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+//practice comparison operator
+const a = 5
+const b =10
+console.log(a==b);
+console.log(a<b);
+console.log(a>b);
+console.log(a<=b);
+console.log(a>=b);
+//practice srting
+const String ="this is a srting"
+console.log(String);
+//joint varriable with sring
+const f = "Hello, ";
+const g = "how are you?";
+const x = `${one}${two}`;
+console.log(x);
+//number and sring
+const word = "abc";
+const number= 123;
+console.log(`${word}${number}`);  
